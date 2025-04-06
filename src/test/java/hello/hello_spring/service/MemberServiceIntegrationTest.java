657f38c338abaa86cef35049dc01af6a35e52857
shlee9999 <oklshop555@naver.com>
2025-04-06 20:49:27 +0900

    ~7-3

--- src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -1,36 +1,40 @@
 package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+// MemberServiceTest 는 Spring 을 이용한 테스트가 아니었다.
+
+@SpringBootTest
 /*
-    단위 테스트가 좋은 테스트일 확률이 높다. 단위 테스트를 잘 만들자.
+ 테스트는 반복이 가능해야한다. DB 에는 Transaction 이라는 개념이 있어서 예를 들어 insert query 를 날리고 commit 을 하기 전에는 반영이 되지 않는다.
+ @Transactional 어노테이션은 DB 데이터를 수정한 후, 테스트가 끝나면 Roll-Back 시켜준다. 즉 commit 하지 않는다.
+ 따라서 테스트 코드의 반복을 가능하게 해주는 매우 유용한 어노테이션이다.
  */
-class MemberServiceTest {
+@Transactional
+class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-/*
-    // MemberService의 memberRepository와 다른 객체이다. 이는 문제가 될 수 있다.
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-*/
-    MemoryMemberRepository memberRepository;
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
+    // field AutoWired 권장하지 않지만, Test 시에는 이렇게 해도 괜찮다.
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+
+    // 이제는 SpringContainer 에게 memberService, memberRepository 를 내놓으라 할 것이라 @BeforeEach 를 지운다.
+
+    // @Transactional 어노테이션을 추가하여 @AfterEach 를 지운다.
+
+
 
     // 테스트는 한국어로 써도 된다.
     // 테스트는 정상 플로우도 중요하지만 예외 플로우가 훨씬 더 중요하다.
